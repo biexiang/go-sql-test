e404c109703f0e7ac762b14170e9abacdf04efb9
ileopold <1547013373@qq.com>
2021-12-12 12:53:35 +0800

    init proj

---
+++ mysql_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"testing"
+)
+
+//  docker pull mysql:5.7
+//  docker run -itd --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
+//  go test -bench .
+func BenchmarkConnectMySQL(b *testing.B) {
+	fnConnectMySQL := func(dsn string) (*sql.DB, error) {
+		return sql.Open("mysql", dsn)
+	}
+	db, err := fnConnectMySQL("root:123456@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		b.Fatalf("mysql connect error : %s", err.Error())
+		return
+	}
+	ctx := context.Background()
+	db.SetMaxIdleConns(-1)
+	b.ResetTimer()
+	b.Run("noConnPool", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			_, _ = db.ExecContext(ctx, "select 1")
+		}
+	})
+	db.SetMaxIdleConns(1)
+	b.ResetTimer()
+	b.Run("hasConnPool", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			_, _ = db.ExecContext(ctx, "select 1")
+		}
+	})
+}
